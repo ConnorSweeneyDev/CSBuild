394585715dcfcbde3ae1248057c6e73a29140600
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-09-24 05:05:58 +0100

    Added scripts to generate compile_commands.

---
+++ script/generate_compile_commands.bat
@@ -0,0 +1,11 @@
+@echo off
+set "CURRENT_DIR=%CD%"
+(
+echo [
+echo   {
+echo     "directory": "%CURRENT_DIR:\=\\%",
+echo     "file": "%CURRENT_DIR:\=\\%\\csb.hpp",
+echo     "command": "clang++ -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -Wshadow-all -Wundef -Wdeprecated -Wtype-limits -Wcast-qual -Wcast-align -Wfloat-equal -Wparentheses -Wunreachable-code-aggressive -Wformat=2"
+echo   }
+echo ]
+) > compile_commands.json
---
+++ script/generate_compile_commands.ps1
@@ -0,0 +1,8 @@
+$CurrentDir = (Get-Location).Path
+$CompileCommand =
+@{
+  directory = $CurrentDir
+  file = "$CurrentDir\csb.hpp"
+  command = "clang++ -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -Wshadow-all -Wundef -Wdeprecated -Wtype-limits -Wcast-qual -Wcast-align -Wfloat-equal -Wparentheses -Wunreachable-code-aggressive -Wformat=2"
+}
+@($CompileCommand) | ConvertTo-Json -Depth 3 -AsArray | Out-File -FilePath "compile_commands.json" -Encoding UTF8
