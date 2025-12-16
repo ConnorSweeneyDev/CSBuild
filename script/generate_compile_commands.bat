1e5be3235504426d82e19ce2fdb6d78181ed6c60
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-12-16 14:34:37 +0000

    Better subprojects.

--- script/generate_compile_commands.bat
+++ script/generate_compile_commands.bat
@@ -6,7 +6,7 @@ echo [
 echo   {
 echo     "directory": "%CURRENT_DIR:\=\\%",
 echo     "file": "%CURRENT_DIR:\=\\%\\csb.hpp",
-echo     "command": "clang++ -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -Wshadow-all -Wundef -Wdeprecated -Wtype-limits -Wcast-qual -Wcast-align -Wfloat-equal -Wunreachable-code-aggressive -Wformat=2"
+echo     "command": "clang++ -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -Wshadow-all -Wundef -Wdeprecated -Wtype-limits -Wcast-qual -Wcast-align -Wfloat-equal -Wunreachable-code-aggressive -Wformat=2 -D_WIN32"
 echo   }
 echo ]
 ) > compile_commands.json
--- script/generate_compile_commands.sh
+++ script/generate_compile_commands.sh
@@ -6,7 +6,7 @@ cat > compile_commands.json << EOF
   {
     "directory": "$current_dir",
     "file": "$current_dir/csb.hpp",
-    "command": "clang++ -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -Wshadow-all -Wundef -Wdeprecated -Wtype-limits -Wcast-qual -Wcast-align -Wfloat-equal -Wunreachable-code-aggressive -Wformat=2"
+    "command": "clang++ -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -Wshadow-all -Wundef -Wdeprecated -Wtype-limits -Wcast-qual -Wcast-align -Wfloat-equal -Wunreachable-code-aggressive -Wformat=2 -D__linux__"
   }
 ]
 EOF
